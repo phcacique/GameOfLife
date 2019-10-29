bff60136ef0305f219e9d2a8000ef2e1e22f08a4
phcacique <phcacique@gmail.com>
2019-10-29 07:39:16 -0300

    Initial Commit

---
+++ GameOfLife/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GameOfLife
+//
+//  Created by Pedro Cacique on 29/10/19.
+//  Copyright © 2019 Pedro Cacique. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
